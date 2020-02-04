59ecae5470b84e7b71e6615aaaad5eb6cdf12bae
caniescape <58758555+caniescape@users.noreply.github.com>
2020-02-04 15:32:59 +0900

    몬스터
    하이

---
+++ monsterManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "monsterManager.h"
+
+
+monsterManager::monsterManager()
+{
+}
+
+
+monsterManager::~monsterManager()
+{
+}
---
+++ monsterManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class monsterManager
+{
+public:
+	monsterManager();
+	~monsterManager();
+};
+
